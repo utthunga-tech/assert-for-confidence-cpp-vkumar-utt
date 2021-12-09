a7f083ac06aa968ea5a1d51fa3c0febae972450c
vkumar-utt <77825470+vkumar-utt@users.noreply.github.com>
2021-12-09 17:07:20 +0530

    Update test-string-calculator.cpp

--- test-string-calculator.cpp
+++ test-string-calculator.cpp
@@ -3,6 +3,18 @@
 #include "test/catch.hpp"
 #include "string-calculator.h"
 
-TEST_CASE("Test Case Name ") {
-   //Write Test 
+TEST_CASE("Test 1: zero number ") {
+  cout << "Test 1 : " <<Add("") 
+}
+
+TEST_CASE("Test 2: 1 number ") {
+  cout << "Test 1 : " <<Add("1") 
+}
+
+TEST_CASE("Test 3: 2 number ") {
+  cout << "Test 1 : " <<Add("1,2") 
+}
+
+TEST_CASE("Test 4: 3 number with multi delimit") {
+  cout << "Test 1 : " <<Add("1\n2,3") 
 }
