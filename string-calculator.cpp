2dc26b425b6d33b3421fbd966141376fa600dff7
vkumar-utt <77825470+vkumar-utt@users.noreply.github.com>
2021-12-09 17:37:09 +0530

    Update string-calculator.cpp

--- string-calculator.cpp
+++ string-calculator.cpp
@@ -1,14 +1,24 @@
 #include "string-calculator.h"
 #include <stdio.h>
 #include <string>
-#include <boost/algorithm/string.hpp>
+
 using namespace std;
 
 vector<string> splitStringMultiDelimit(string inputString)
 {
-    string delimiters("\n,");
+    std::string const delims{ "\n," };
+
     vector<string> strNums;
-    boost::split(strNums, inputString, boost::is_any_of(delimiters));
+
+    size_t beg, pos = 0;
+    while ((beg = inputString.find_first_not_of(delims, pos)) != std::string::npos)
+    {
+        pos = inputString.find_first_of(delims, beg + 1);
+        string strnum = inputString.substr(beg, pos - beg);
+        std::cout <<strnum  << std::endl;
+
+        strNums.push_back(strnum);
+    }
 
     return strNums;
 }
